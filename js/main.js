8407820f97faccf116c9fc30b99f350c09ec2fb6
Ezegallard0 <plumiferogaming@gmail.com>
2024-04-14 18:38:16 -0300

    cambiando el diseño

--- js/main.js
+++ js/main.js
@@ -7,3 +7,79 @@ burguer.addEventListener("change",function(){
         modal.style.visibility="hidden"
     }
 })
+document.addEventListener("DOMContentLoaded", function () {
+    const carrito = []; // array que vamos a usar para agregar los productos
+
+    function agregarAlCarrito(producto) { // declaramos función de agregar producto al carrito 
+        carrito.push(producto); // método del array para agregar un producto al carrito
+        actualizarCarrito(); // invocamos la función para que lea y actualice qué hay en nuestro carrito
+    }
+
+    function eliminarDelCarrito(index) { // declaramos función que elimina un producto del carrito
+        carrito.splice(index, 1); // método del array para eliminar elemento del array
+        actualizarCarrito();
+    }
+
+    function actualizarCarrito() {
+        const carritoElemento = document.getElementById("carrito");
+        const totalElemento = document.getElementById("total");
+
+        carritoElemento.innerHTML = ""; // blanquear carrito
+
+        let total = 0; // esta variable actúa como acumulador;
+
+        carrito.forEach(function (producto, index) { // recorriendo los productos del carrito
+            const li = document.createElement("li");
+            li.textContent = producto.nombre + "-$" + producto.precio.toFixed(2);
+            li.style.listStyle = "none";
+
+            const botonEliminar = document.createElement("button"); // creamos el botón eliminar
+            botonEliminar.textContent = "Eliminar";
+            botonEliminar.style.marginLeft = "20%";
+            botonEliminar.addEventListener("click", function () { // creamos la función para eliminar un solo producto
+                eliminarDelCarrito(index);
+            });
+
+            li.appendChild(botonEliminar);
+            carritoElemento.appendChild(li);
+
+            total += producto.precio;
+        });
+
+        totalElemento.textContent = total.toFixed(2);
+    }
+
+    function vaciarCarrito() {
+        carrito.length = 0;
+        actualizarCarrito();
+    }
+
+    // traemos los botones
+    const botonComprar = document.querySelectorAll(".agregar");
+    const botonVaciarCarrito = document.querySelector("#vaciar_carrito");
+
+    // agregar los eventos para que funcionen los botones
+
+    botonComprar.forEach(function (boton, index) {
+        boton.addEventListener("click", function () {
+            const nombre = document.querySelectorAll("b")[index].textContent;
+            const precioElemento = document.getElementById("precio-producto-" + (index + 1));
+            const precio = parseFloat(precioElemento.textContent.replace("$", ""));
+
+            agregarAlCarrito({ nombre, precio });
+        });
+    });
+
+    botonVaciarCarrito.addEventListener("click", vaciarCarrito);
+
+    const carritoIcono = document.getElementById("carrito-icono");
+    const sectionCarrito = document.getElementById("div_carrito");
+
+    carritoIcono.addEventListener("click", function () {
+        if (sectionCarrito.style.display === "none") {
+            sectionCarrito.style.display = "block";
+        } else {
+            sectionCarrito.style.display = "none";
+        };
+    });
+});
